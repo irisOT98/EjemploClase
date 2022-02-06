53e3d70d8281d0411586253b151390611f5ae733
Iris Olvera Developer <iris.oeti98@gmail.com>
2022-02-06 10:56:30 -0600

    1° commit

---
+++ src/test/java/com/example/ClaseSpring/EjemploClase/EjemploClaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ClaseSpring.EjemploClase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploClaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
